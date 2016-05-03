a785c2868c97f2b14a1c9fcad5b3e9a6c7f4d931
student1 <student1@jh-mbair-158-02.d214.org>
2016-05-03 09:46:09 -0500

    Finished Block class

--- Breakout/Block.swift
+++ Breakout/Block.swift
@@ -11,6 +11,36 @@ import UIKit
 class Block
 {
     var paddle: UIView
+    var ball: UIView
+    var block1: UIView
+    var block2: UIView
+    var block3: UIView
+    var block4: UIView
+    var block5: UIView
+    
+    init(Paddle p: UIView, Ball b: UIView, Block1 b1: UIView, Block2 b2: UIView, Block3 b3: UIView, Block4 b4: UIView, Block5 b5: UIView)
+    {
+        paddle = p
+        ball = b
+        block1 = b1
+        block2 = b2
+        block3 = b3
+        block4 = b4
+        block5 = b5
+        
+    }
+    init()
+    {
+        paddle = UIView()
+        ball = UIView()
+        block1 = UIView()
+        block2 = UIView()
+        block3 = UIView()
+        block4 = UIView()
+        block5 = UIView()
+        
+        
+    }
     
     
     
