9c801877cc1d5b5243e602fca6032a6479c6357b
student1 <student1@jh-mbair-158-02.d214.org>
2016-05-03 09:37:39 -0500

    Setting up the class block

---
+++ Breakout/Block.swift
@@ -0,0 +1,24 @@
+//
+//  Block.swift
+//  Breakout
+//
+//  Created by student1 on 5/3/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class Block
+{
+    var paddle: UIView
+    
+    
+    
+
+    
+
+    }
+    
+
+   
+
