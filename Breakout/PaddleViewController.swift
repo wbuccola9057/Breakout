ef4b9d1bccf5ec39aaba211096b0a5c80ca59833
student1 <student1@jh-mbair-158-05.d214.org>
2016-05-06 09:32:26 -0500

    working on pan gesture on paddle

--- Breakout/PaddleViewController.swift
+++ Breakout/PaddleViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class PaddleViewController: UIViewController {
-
+    
+    let paddle = UIView(frame: CGRect(x: 300, y: 900, width: 180, height: 15))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        let paddle = UIView(frame: CGRect(x: 300, y: 900, width: 180, height: 15))
         paddle.backgroundColor = UIColor.blueColor()
         view.addSubview(paddle)
-        
-        
-        var imageView : UIImageView
-        imageView  = UIImageView(frame: CGRect(x: 385, y: 875, width: 25, height: 25))
-        imageView.image = UIImage(named:"ball")
-        self.view.addSubview(imageView)
 
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(PaddleViewController.handleTap(_:)))
+        let tapGesture = UIPanGestureRecognizer(target: self, action: Selector("onCustonPan"))
+        //let tapGesture = UITapGestureRecognizer(target: self, action: Selector("handleTap:"))
         paddle.addGestureRecognizer(tapGesture)
         
         func handleTap(sender: UIGestureRecognizer) {
@@ -34,7 +28,11 @@ class PaddleViewController: UIViewController {
         
         print("Tapped")
     }
-    
+        
+        var imageView : UIImageView
+        imageView  = UIImageView(frame: CGRect(x: 385, y: 875, width: 25, height: 25))
+        imageView.image = UIImage(named:"ball")
+        self.view.addSubview(imageView)
     
 
     
