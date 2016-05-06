eef7962fce8034a4a4e2990a39b550ce18aee115
student1 <student1@jh-mbair-158-04.d214.org>
2016-05-06 09:41:46 -0500

    added blocks

--- Breakout/PaddleViewController.swift
+++ Breakout/PaddleViewController.swift
@@ -26,9 +26,25 @@ class PaddleViewController: UIViewController {
         imageView.image = UIImage(named:"ball")
         self.view.addSubview(imageView)
         
-        let block1 = UIView(frame: CGRect(x: 50, y: 100, width: 100, height: 50))
+        let block1 = UIView(frame: CGRect(x: 5, y: 100, width: 150, height: 25))
         block1.backgroundColor = UIColor.redColor()
         view.addSubview(block1)
+        
+        let block2 = UIView(frame: CGRect(x: 160, y: 100, width: 150, height: 25))
+        block2.backgroundColor = UIColor.redColor()
+        view.addSubview(block2)
+        
+        let block3 = UIView(frame: CGRect(x: 315, y: 100, width: 150, height: 25))
+        block3.backgroundColor = UIColor.redColor()
+        view.addSubview(block3)
+        
+        let block4 = UIView(frame: CGRect(x: 470, y: 100, width: 150, height: 25))
+        block4.backgroundColor = UIColor.redColor()
+        view.addSubview(block4)
+        
+        let block5 = UIView(frame: CGRect(x: 625, y: 100, width: 140, height: 25))
+        block5.backgroundColor = UIColor.redColor()
+        view.addSubview(block5)
 
         
     }
