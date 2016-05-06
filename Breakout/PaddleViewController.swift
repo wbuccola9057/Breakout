7746154c635022442c76e69d8d00f2b30536e4bd
student1 <student1@jh-mbair-158-05.d214.org>
2016-05-06 09:57:07 -0500

    attempted to do the pan gesture on story board

--- Breakout/PaddleViewController.swift
+++ Breakout/PaddleViewController.swift
@@ -10,23 +10,25 @@ import UIKit
 
 class PaddleViewController: UIViewController {
     
-    let paddle = UIView(frame: CGRect(x: 300, y: 900, width: 180, height: 15))
     
+    @IBOutlet weak var paddleView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        paddle.backgroundColor = UIColor.blueColor()
-        view.addSubview(paddle)
+        
+            //let paddle = UIView(frame: CGRect(x: 300, y: 900, width: 180, height: 15))
+           // paddle.backgroundColor = UIColor.blueColor()
+           // view.addSubview(paddle)
 
+        //func handleTap(sender: UIGestureRecognizer) {
+        //    let point = sender.locationInView(view)
+        //    paddle.center = CGPointMake(point.x, point.y)
+      //  print("Tapped")
+        //}
         
-        func handleTap(sender: UIGestureRecognizer) {
-            let point = sender.locationInView(view)
-            paddle.center = CGPointMake(point.x, point.y)
-
         
         
-        print("Tapped")
-    }
+        
         //ball
         var imageView : UIImageView
         imageView  = UIImageView(frame: CGRect(x: 385, y: 875, width: 25, height: 25))
@@ -45,5 +47,7 @@ class PaddleViewController: UIViewController {
     }
    
     @IBAction func paddleMovement(sender: AnyObject) {
+       
+
     }
 }
