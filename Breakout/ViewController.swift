f9c7630a004d39d1fc9d942a97e6e0349503331b
student1 <student1@jh-mbair-158-01.d214.org>
2016-05-13 09:57:55 -0500

    Altered size of Breakout label and added bounce intro animation effect for play button

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -13,9 +13,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PauseBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
       NSThread.sleepForTimeInterval(2)
+        
+        UIView.animateWithDuration(1.0, delay: 0, usingSpringWithDamping: 2.0, initialSpringVelocity: 15, options: UIViewAnimationOptions.CurveEaseInOut, animations: ({
+            self.PauseBtn.center.x = self.view.frame.width
+        }), completion: nil)
     
     }
 }
